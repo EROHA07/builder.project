a6309960e132e4a71f156d125ff39757e3051e08
erasy <erkingalijseitov@gmail.com>
2026-09-13 18:12:01 +0500

    Add README and UML documentation

--- src/pizzabuilder/Pizza.java
+++ src/pizzabuilder/Pizza.java
@@ -2,24 +2,25 @@ package pizzabuilder;
 
 import java.util.List;
 
-public class pizza {
+public class Pizza {
     private final String dough;
     private final String sauce;
     private final String cheese;
     private final List<String> toppings;
     private final String size;
 
-    public pizza(String dough, String sauce, String cheese, List<String> toppings, String size) {
+    public Pizza(String dough, String sauce, String cheese,
+                 List<String> toppings, String size) {
         this.dough = dough;
         this.sauce = sauce;
         this.cheese = cheese;
-        this.toppings = toppings;
+        this.toppings = List.copyOf(toppings);
         this.size = size;
     }
 
     @Override
     public String toString() {
-        return "Pizza {" +
+        return "Pizza{" +
                 "dough='" + dough + '\'' +
                 ", sauce='" + sauce + '\'' +
                 ", cheese='" + cheese + '\'' +
